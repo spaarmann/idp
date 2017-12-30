68d758b065579a60fba9bdd1a7c2e9524cdd3492
Sebastian Paarmann <sebastian.paarmann@gmail.com>
2017-12-30 22:35:46 +0100

    Initial server component, basic loading of values from DB

---
+++ server/README.md
@@ -0,0 +1,6 @@
+Server
+======
+
+The `server` component provides a web interface to display measurements collected during reactor operations.
+
+The data is uploaded by the `uploader` component.
---
+++ server/values.php
@@ -0,0 +1,39 @@
+<?php
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE); ini_set('display_errors', '1');
+
+$username = getenv("idp_sql_user");
+$password = getenv("idp_sql_pass");
+$hostname = "localhost";
+$database = "idp";
+
+$mysqli = new mysqli($hostname, $username, $password, $database);
+if ($mysqli->connect_errno) {
+    die("Failed to connect to database! " . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+}
+
+if (!($stmt = $mysqli->prepare("SELECT time, value FROM temperature WHERE time BETWEEN ? AND ?"))) {
+    die("Failed to prepare statement! " . $mysqli->errno . ") " . $mysqli->error);
+}
+
+$start_time = urldecode($_GET['start']);
+$end_time = urldecode($_GET['end']);
+
+if (!$stmt->bind_param("ss", $start_time, $end_time)) {
+    die("Failed to bind params! " . $mysqli->errno . ") " . $mysqli->error);
+}
+
+if (!$stmt->execute()) {
+    die("Failed to execute statement! " . $mysqli->errno . ") " . $mysqli->error);
+}
+
+if ($result = $stmt->get_result()) {
+    while ($row = $result->fetch_assoc()) {
+        printf("%s => %s°C<br />", $row["time"], $row["value"]);
+    }
+    $result->free();
+}
+
+$stmt->close();
+$mysqli->close();
+?>
