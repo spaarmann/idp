b4fd48b2995443710cdfbd219cd658705f3d8bd2
Sebastian Paarmann <sebastian.paarmann@gmail.com>
2017-12-31 18:53:01 +0100

    Add PH values

--- server/api/values.php
+++ server/api/values.php
@@ -12,35 +12,39 @@ if ($mysqli->connect_errno) {
     die("Failed to connect to database! " . $mysqli->connect_errno . ") " . $mysqli->connect_error);
 }
 
-if (!($stmt = $mysqli->prepare("SELECT time, value FROM temperature WHERE time BETWEEN ? AND ?"))) {
-    die("Failed to prepare statement! " . $mysqli->errno . ") " . $mysqli->error);
-}
-
 $start_time = urldecode($_GET['start']);
 $end_time = urldecode($_GET['end']);
 
-if (!$stmt->bind_param("ss", $start_time, $end_time)) {
-    die("Failed to bind params! " . $mysqli->errno . ") " . $mysqli->error);
-}
 
-if (!$stmt->execute()) {
-    die("Failed to execute statement! " . $mysqli->errno . ") " . $mysqli->error);
-}
-
-if ($result = $stmt->get_result()) {
-    $json = "[";
-
-    while ($row = $result->fetch_assoc()) {
-        //printf('{ "time": "%s", "value": "%s"}, ', $row["time"], $row["value"]);
-        $json = $json . '{ "time": "' . $row["time"] . '", "value": "' . $row["value"] . '"},';
+if ($step = $_GET['valuegen-step']) {
+    
+} else {
+    if (!($stmt = $mysqli->prepare("SELECT time, temperature, ph FROM measurements WHERE time BETWEEN ? AND ?"))) {
+        die("Failed to prepare statement! " . $mysqli->errno . ") " . $mysqli->error);
     }
 
-    $json = substr($json, 0, strlen($json)-1);
-    $json = $json . ']';
+    if (!$stmt->bind_param("ss", $start_time, $end_time)) {
+        die("Failed to bind params! " . $mysqli->errno . ") " . $mysqli->error);
+    }
 
-    echo $json;
+    if (!$stmt->execute()) {
+        die("Failed to execute statement! " . $mysqli->errno . ") " . $mysqli->error);
+    }
 
-    $result->free();
+    if ($result = $stmt->get_result()) {
+        $json = "[";
+
+        while ($row = $result->fetch_assoc()) {
+            $json = $json . '{ "time": "' . $row["time"] . '", "temp": "' . $row["temperature"] . '", "ph": "' . $row["ph"] . '"},';
+        }
+
+        $json = substr($json, 0, strlen($json)-1);
+        $json = $json . ']';
+
+        echo $json;
+
+        $result->free();
+    }
 }
 
 $stmt->close();
